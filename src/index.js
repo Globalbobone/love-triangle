4a0671bc28b37d7fb5128378dcd8d0f618cc54f3
Aliaksei Pliakhanau <3253255@rambler.ru>
2018-02-20 18:10:00 +0300

    first 38 tests

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var arr = preferences; 
+  var count = 0;
+    for (var i = 0; i < arr.length; i++){
+    var a;
+    var b;
+    var c;
+    var d;
+    var e;
+    var f;
+    a = arr[i];
+    b = i + 1; 
+    c = arr.indexOf(b)+1;
+    d = arr[c-1];
+    e = arr.indexOf(c)+1;
+    f = arr[e-1];
+
+    
+    if (a == e && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
+      count++;
+      delete arr[b-1];
+      delete arr[c-1];
+      delete arr[e-1]
+    };
+  }  
+  return count;
 };
+  
+ 
