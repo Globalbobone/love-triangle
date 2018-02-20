2cf2949abe3d3e08e8b6374e0d55d01fd31ba898
Aliaksei Pliakhanau <3253255@rambler.ru>
2018-02-20 20:12:52 +0300

    some else tests

--- src/index.js
+++ src/index.js
@@ -5,28 +5,81 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
   var arr = preferences; 
   var count = 0;
-    for (var i = 0; i < arr.length; i++){
-    var a;
-    var b;
-    var c;
-    var d;
-    var e;
-    var f;
+  var len = arr.length;
+    for (var i = 0; i < len; i++){
+    var a, b, c, d, e, f;
     a = arr[i];
     b = i + 1; 
     c = arr.indexOf(b)+1;
     d = arr[c-1];
     e = arr.indexOf(c)+1;
     f = arr[e-1];
-
     
-    if (a == e && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
+    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
       count++;
       delete arr[b-1];
       delete arr[c-1];
       delete arr[e-1]
+    }
+    else {
+      delete arr[e-1];
     };
   }  
+  for (var i = len; i > 0; i--){
+    a = arr[i];
+    b = i + 1; 
+    c = arr.indexOf(b)+1;
+    d = arr[c-1];
+    e = arr.indexOf(c)+1;
+    f = arr[e-1];
+    
+    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
+      count++;
+      delete arr[b-1];
+      delete arr[c-1];
+      delete arr[e-1]
+    }
+    else {
+      delete arr[e-1];
+    };
+  }
+  for (var i = 0; i < len; i++){
+    a = arr[i];
+    b = i + 1; 
+    c = arr.indexOf(b)+1;
+    d = arr[c-1];
+    e = arr.indexOf(c)+1;
+    f = arr[e-1];
+    
+    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
+      count++;
+      delete arr[b-1];
+      delete arr[c-1];
+      delete arr[e-1]
+    }
+    else {
+      delete arr[c-1];
+    };
+  }
+  for (var i = len; i > 0; i--){
+    a = arr[i];
+    b = i + 1; 
+    c = arr.indexOf(b)+1;
+    d = arr[c-1];
+    e = arr.indexOf(c)+1;
+    f = arr[e-1];
+    
+    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
+      count++;
+      delete arr[b-1];
+      delete arr[c-1];
+      delete arr[e-1]
+    }
+    else {
+      delete arr[e-1];
+    };
+  }
+      
   return count;
 };
   
