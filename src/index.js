66e40d725f076cab2f1bdb0320e953d12a563ee9
Aliaksei Pliakhanau <3253255@rambler.ru>
2018-02-23 01:35:36 +0300

    add final solution

--- src/index.js
+++ src/index.js
@@ -1,86 +1,43 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   var arr = preferences; 
-  var count = 0;
+  let count = 0;
   var len = arr.length;
-    for (var i = 0; i < len; i++){
-    var a, b, c, d, e, f;
-    a = arr[i];
-    b = i + 1; 
-    c = arr.indexOf(b)+1;
-    d = arr[c-1];
-    e = arr.indexOf(c)+1;
-    f = arr[e-1];
-    
-    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
-      count++;
-      delete arr[b-1];
-      delete arr[c-1];
-      delete arr[e-1]
+    for (var i = 1; i <= len; i++){
+    let a, b, c;
+    a = arr[i]-1;
+    b = arr[a]-1; 
+    c = arr[b];
+    if (arr[i] == i+1){
+      continue;
     }
-    else {
-      delete arr[e-1];
+
+    if (c == i+1){ 
+      count++;
+      delete arr[i];
     };
   }  
-  for (var i = len; i > 0; i--){
-    a = arr[i];
-    b = i + 1; 
-    c = arr.indexOf(b)+1;
-    d = arr[c-1];
-    e = arr.indexOf(c)+1;
-    f = arr[e-1];
-    
-    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
-      count++;
-      delete arr[b-1];
-      delete arr[c-1];
-      delete arr[e-1]
-    }
-    else {
-      delete arr[e-1];
-    };
-  }
-  for (var i = 0; i < len; i++){
-    a = arr[i];
-    b = i + 1; 
-    c = arr.indexOf(b)+1;
-    d = arr[c-1];
-    e = arr.indexOf(c)+1;
-    f = arr[e-1];
-    
-    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
-      count++;
-      delete arr[b-1];
-      delete arr[c-1];
-      delete arr[e-1]
-    }
-    else {
-      delete arr[c-1];
-    };
-  }
-  for (var i = len; i > 0; i--){
-    a = arr[i];
-    b = i + 1; 
-    c = arr.indexOf(b)+1;
-    d = arr[c-1];
-    e = arr.indexOf(c)+1;
-    f = arr[e-1];
-    
-    if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
-      count++;
-      delete arr[b-1];
-      delete arr[c-1];
-      delete arr[e-1]
-    }
-    else {
-      delete arr[e-1];
-    };
-  }
-      
   return count;
 };
-  
- 
+
+
+//   for (var i = len; i > 0; i--){
+//     a = arr[i];
+//     b = i + 1; 
+//     c = arr.indexOf(b)+1;
+//     d = arr[c-1];
+//     e = arr.indexOf(c)+1;
+//     f = arr[e-1];
+//     if (a==e && b==d && c==f && a!=b && d!=c && f!=e && a!=d && d!=f && a!=f){ 
+//       count++;
+//       delete arr[b-1];
+//       delete arr[c-1];
+//       delete arr[e-1]
+//     }
+//     else {
+//       delete arr[e-1];
+//     };
+//   }
+//   return count;
+// };
+
+
